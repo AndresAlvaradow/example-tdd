84f5c3795b9015e367527a8e19f0f1b7287fe900
AndresAlvaradow <andresalvarado34@gmail.com>
2025-07-01 13:19:21 -0500

    Primer commit

---
+++ roman_numeral.spec.ts
@@ -0,0 +1,7 @@
+import { romanNumber } from "./roman_numeral";
+describe("romanNumber", () => {
+    it("should return the same number", () => {
+        expect(romanNumber("I")).toBe("I");
+    });
+});
+
---
+++ roman_numeral.ts
@@ -0,0 +1,5 @@
+export function romanNumber (number: string): string {
+    return number;
+}
+
+// const 
